2c26696b3f7d4f8e3b75996a8b9e684119142be6
Shadhin <79417172+Shahariarshadhin@users.noreply.github.com>
2023-10-07 00:15:45 +0600

    Update HomeContainer.jsx

--- components/Home/HomeContainer.jsx
+++ components/Home/HomeContainer.jsx
@@ -1,7 +1,7 @@
 
 const HomeContainer = () => {
   return (
-    <div className="text-5xl text-white font-bold flex justify-center mt-[12%]">GeoMag Gaurd</div>
+    <div className="text-5xl text-white font-bold flex justify-center mt-[12%]">GeoMag Guard</div>
   )
 }
 
